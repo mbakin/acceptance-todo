c11d6b1ebad699aba0dc468b7891295cf50eb33e
mbakin <mehmetberkakinn@outlook.com>
2022-02-25 16:49:53 +0300

    completed first scenario

--- features/steps/web-based-todo.js
+++ features/steps/web-based-todo.js
@@ -2,7 +2,9 @@ const {Given, When, Then} = require("cucumber");
 const openUrl = require('../support/action/openUrl');
 const waitForSelector = require("../support/action/waitForSelector");
 const checkElementExists = require("../support/check/checkElementExists");
-
+const sendKeys = require("../support/action/sendKeys");
+const clickElement = require("../support/action/clickElement");
+const assert = require("assert")
 
 Given(/^Empty ToDo list$/,async function () {
   await openUrl.call(this, "/");
@@ -12,9 +14,24 @@ Given(/^Empty ToDo list$/,async function () {
   
 });
 
-When(/^I write "([^"]*)" to (.*) and click to (.*)$/, async function (dummyTodo) {
-  await sendKeys.call(this, "#add-todo-input", dummyTodo)
-});
-Then(/^I should see "([^"]*)" item in ToDo list$/, async function () {
 
+When(/^I write "([^"]*)" to text box and click to add button$/, async function (dummyTodo) {
+    await sendKeys.call(this, "#add-todo-input", dummyTodo)
+    await clickElement.call(this, "#add-todo-button")
+});
+
+Then(/^I should see "([^"]*)" item in ToDo list$/, async function (dummy) {
+  const selector = ".layout"
+  let todo = await this.page.$$eval(
+    selector,
+    async (todoItems, dummy) => {
+      let todoItemExist = todoItems.
+      find(item => item.querySelector(".todos"))
+      const text = todoItemExist.querySelector("li").textContent.includes(dummy)
+      console.log(text)
+      return !!todoItemExist
+    },
+    dummy
+  )
+  assert.strictEqual(todo,true)
 });
